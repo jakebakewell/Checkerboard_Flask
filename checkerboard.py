15da827bdd68ed655584ab5870c8c5774309b47a
unknown <bakewelljake@gmail.com>
2021-03-12 10:40:24 -0600

    added a new route and swapped x and y variables

--- checkerboard.py
+++ checkerboard.py
@@ -5,17 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', rows = 8, columns = 8, color0 = "black", color1 = "red")
 
-@app.route('/<x>')
-def specify_num_rows(x):
-    return render_template('index.html', rows = int(x), columns = 8, color0 = "black", color1 = "red")
+@app.route('/<y>')
+def specify_num_rows(y):
+    return render_template('index.html', rows = int(y), columns = 8, color0 = "black", color1 = "red")
 
-@app.route('/<x>/<y>')
-def specify_num_rows_columns(x, y):
-    return render_template('index.html', rows =  int(x), columns = int(y), color0 = "black", color1 = "red")
+@app.route('/<y>/<x>')
+def specify_num_rows_columns(y, x):
+    return render_template('index.html', rows =  int(y), columns = int(x), color0 = "black", color1 = "red")
 
-@app.route('/<x>/<y>/<color0>/<color1>')
-def specify_checkerboard_traits(x, y, color0, color1):
-    return render_template('index.html', rows =  int(x), columns = int(y), color0 = color0, color1 = color1)
+@app.route('/<y>/<x>/<color0>')
+def specify_checkerboard_color(y, x, color0):
+    return render_template('index.html', rows =  int(y), columns = int(x), color0 = color0, color1 = "red")
+
+@app.route('/<y>/<x>/<color0>/<color1>')
+def specify_checkerboard_traits(y, x, color0, color1):
+    return render_template('index.html', rows =  int(y), columns = int(x), color0 = color0, color1 = color1)
 
 if __name__ == '__main__':
     app.run(debug=True)
